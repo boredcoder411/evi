124a45a94e358c43c1e1b7265cb4032faebff1f0
boredcoder411 <105937655+boredcoder411@users.noreply.github.com>
2022-07-09 17:44:04 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+const socket = io('https://eviserver.boredcoder411.repl.co');
+
+socket.on("connect", () => {
+  console.log(socket.id);
+  const usrname = document.querySelector("#usrname")
+  usrname.innerHTML = "Your player id for this session is: " + socket.id;
+});
+
+socket.on('message', text => {
+
+    const el = document.createElement('li');
+    el.innerHTML = text;
+    document.querySelector('ul').appendChild(el)
+
+});
+
+document.querySelector('button').onclick = () => {
+
+    const text = document.querySelector('input').value;
+    document.querySelector('input').value = "";
+    socket.emit('message', text)
+    
+}
